f3f04eab3051d25b25d30d9b4a8b8902aa84f329
Diego Fraga <51933800+Dipedro87@users.noreply.github.com>
2023-01-27 18:45:58 -0300

    Botão flutuante
    Adicionada a lógica do botão flutuante.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// add classe na barra de navegação
-
+// add funcao na barra horizontal
 const header = document.querySelector('.navbar');
 
 window.onscroll = function() {
@@ -11,4 +10,28 @@ window.onscroll = function() {
         header.classList.remove('navbarDark');
     }
 }
+
+// botão flutuante 
+let floatForm = document.querySelector('.float-form');
+let floatButton = document.querySelector('.float-form-button');
+
+floatButton.addEventListener('click', function() {
+    floatForm.style.display = (floatForm.style.display === 'none') ? 'block' : 'none';
+});
+
+// validação do e-mail
+let emailInput = document.querySelector('input[type=email]');
+let submitButton = document.querySelector('input[type=submit]');
+let form = document.querySelector('.float-form');
+
+submitButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  let email = emailInput.value;
+  let emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  if (!emailRegex.test(email)) {
+    alert('Entre com seu endereço válido');
+  } else {
+    window.location.href = form.action + '?email=' + email;
+  }
+});
 
